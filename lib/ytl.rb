0ef607a26c20d01f57a6224d5001f4ed2ee7a1fe
Miura Hideki <m-72@tf6.so-net.ne.jp>
2010-10-20 21:22:03 +0900

    rewrite as frontend of ytljit

--- lib/ytl.rb
+++ lib/ytl.rb
@@ -1,1 +1,32 @@
-require 'ytl/ast'
+require 'ytljit'
+require 'pp'
+
+include YTLJit
+
+opts = {  
+  :peephole_optimization    => true,
+  :inline_const_cache       => false,
+  :specialized_instruction  => false,}
+
+
+is = RubyVM::InstructionSequence.compile(File.read(ARGV[0]), ARGV[0], "", 0,
+                                         opts).to_a
+iseq = VMLib::InstSeqTree.new(nil, is)
+# pp iseq
+
+tr = VM::YARVTranslatorSimple.new([iseq])
+tnode = tr.translate
+ci_context = VM::CollectInfoContext.new(tnode)
+tnode.collect_info(ci_context)
+
+ti_context = VM::TypeInferenceContext.new(tnode)
+begin
+  tnode.collect_candidate_type(ti_context, [], [])
+end until ti_context.convergent
+ti_context = tnode.collect_candidate_type(ti_context, [], [])
+
+c_context = VM::CompileContext.new(tnode)
+c_context = tnode.compile(c_context)
+tcs = tnode.code_space
+tcs.call(tcs.base_address)
+
---
+++ test/basictest.rb
@@ -0,0 +1,33 @@
+def id (x)
+  x
+end
+
+p id(1)
+p id(1.9)
+
+def array
+  [1, 2, 3][0] + [1, 2, 3][1]  
+end
+p array
+
+def fib(x)
+  if x < 2 then
+    1
+  else
+    fib(x + -1) + fib(x + -2)
+  end
+end
+
+p fib(35)
+
+def blk0(x)
+  yield(x) + 2
+end
+
+def blk1(x)
+  yield(x) + 10
+end
+
+p blk0(1) {|a| a + 1}
+p blk1(1) {|a| blk0(a) {|b| b + a}}
+  
