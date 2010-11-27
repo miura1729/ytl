097e8d71051d5e61badfaaaf71d5dfa63df0a939
Miura Hideki <m-72@tf6.so-net.ne.jp>
2010-11-28 07:11:10 +0900

    Add import object Math

--- lib/ytl/importobj.rb
+++ lib/ytl/importobj.rb
@@ -4,6 +4,7 @@ module YTL
   def self.import_ruby_object(context)
     context.import_object(Object, :Array, Array) 
     context.import_object(Object, :String, String)
+    context.import_object(Object, :Math, Math)
   end
 end
   
