c7b4097af3e1d73a0d2c9bd8b416e1af0421b737
Miura Hideki <m-72@tf6.so-net.ne.jp>
2010-11-28 21:08:00 +0900

    Delete debug print

--- runtime/type.rb
+++ runtime/type.rb
@@ -1,6 +1,5 @@
 include YTLJit
 include InternalRubyType
-p Runtime::Arena.new.address
 
 tr_context.import_object(YTLJit::AsmType, :VALUE, VALUE)
 tr_context.import_object(YTLJit::AsmType, :P_CHAR, P_CHAR)
