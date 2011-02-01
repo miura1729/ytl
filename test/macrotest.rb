0db24742dab9c302ec6948a43219efbe904baa2c
Miura Hideki <m-72@tf6.so-net.ne.jp>
2011-02-02 01:40:08 +0900

    Add macro test

--- test/macrotest.rb
+++ test/macrotest.rb
@@ -1,11 +1,20 @@
-def myattr(*x)
-  str = ""
-  x.each do |e|
-     eval "def #{e}; @#{e}; end\n"
+class Module
+  def myattr(*x)
+    str = ""
+    x.each do |e|
+      eval "def #{e}; @#{e}; end\n"
+    end
+  end
+end
+
+class Foo
+  def myattr(x)
+    p x
   end
 end
 
 myattr :foo, :bar
+a = Foo.new.myattr("abc")
 
 # Can't psss this test yet
 =begin
