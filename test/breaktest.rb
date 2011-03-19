981a952375b333b893bb0e2da6113d1be72de0b5
Miura Hideki <m-72@tf6.so-net.ne.jp>
2011-03-19 16:29:20 +0900

    Modify breaktest for debug easy

--- test/breaktest.rb
+++ test/breaktest.rb
@@ -2,7 +2,7 @@ def test0
   begin
     return
   ensure
-    p "BAR"
+    p "BAR0"
   end
 end
 
@@ -11,7 +11,7 @@ def test1
     p n
     next
   end
-  p "foo"
+  p "foo1"
 end
 
 def test2
@@ -19,7 +19,7 @@ def test2
     p n
     return
   end
-  p "foo"
+  p "bar2"
 end
 
 def test3
@@ -28,11 +28,24 @@ def test3
       p n
       return
     end
-    p "foo"
+    p "foo3"
   ensure
-    p "bar"
+    p "bar3"
   end
 end
+
+def test4
+  begin
+    [1, 2, 3].each do |n|
+      p n
+      break
+    end
+    p "foo4"
+  ensure
+    p "bar4"
+  end
+end
+
 
 test0
 
@@ -40,3 +53,32 @@ test1
 test2
 
 test3
+test4
+
+=begin
+# Not support yet
+
+def test5
+  begin
+    [1, 2, 3].each do |n|
+      p n
+      next
+    end
+    p "foo5"
+  ensure
+    p "bar5"
+  end
+end
+
+def test6
+  begin
+    [1, 2, 3].each do |n|
+      p n
+      redo
+    end
+    p "foo"
+  ensure
+    p "bar"
+  end
+end
+=end
