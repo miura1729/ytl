1834feaede3a16b51cf62ca60327a3ddf8e57717
Miura Hideki <m-72@tf6.so-net.ne.jp>
2011-04-21 21:14:26 +0900

    Add test for if_sepc

--- spec/if_spec.rb
+++ spec/if_spec.rb
@@ -10,4 +10,29 @@ describe "Fixnum#+" do
     prog = "a = []; if false then a = [123]; end; a"
     prog.execute_ytl.should == prog.execute_ruby
   end
+
+  it "does not evaluate body if expression is false" do
+    prog = "a = []; if false then a = [123]; end; a"
+    prog.execute_ytl.should == prog.execute_ruby
+  end
+
+  it "does not evaluate body if expression is true" do
+    prog = "a = []; if () then a = [123]; end; a"
+    prog.execute_ytl.should == prog.execute_ruby
+  end
+
+  it "does not evaluate else-body if expression is true" do
+    prog = "a = []; if true then a = [123]; else [456]; end; a"
+    prog.execute_ytl.should == prog.execute_ruby
+  end
+
+  it "evaluate only else-body if expression is false" do
+    prog = "a = []; if false then a = [123]; else [456]; end; a"
+    prog.execute_ytl.should == prog.execute_ruby
+  end
+
+  it "returns result of then-body evaluation if expression is true" do
+    prog = "if true then 123 end"
+    prog.execute_ytl.should == prog.execute_ruby
+  end
 end
