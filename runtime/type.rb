8273ec81ec2d71842096605c67cee9082443387c
Miura Hideki <m-72@tf6.so-net.ne.jp>
2011-12-17 23:27:19 +0900

    Add Ytl/Ytl::AsmType/ Ytl::Runtime module itself

--- runtime/type.rb
+++ runtime/type.rb
@@ -1,6 +1,8 @@
 include YTLJit
 include InternalRubyType
 
+tr_context.import_object(YTLJit, :AsmType, YTLJit::AsmType)
+tr_context.import_object(YTLJit, :Runtime, YTLJit::Runtime)
 tr_context.import_object(YTLJit::AsmType, :VALUE, VALUE)
 tr_context.import_object(YTLJit::AsmType, :P_CHAR, P_CHAR)
 tr_context.import_object(YTLJit::AsmType, :RBasic, RBasic)
