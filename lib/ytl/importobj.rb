4fc08ac9b488110c1549bd0553204b2ed739d53d
Miura Hideki <m-72@tf6.so-net.ne.jp>
2012-11-13 23:16:33 +0900

    Add imported Ruby class

--- lib/ytl/importobj.rb
+++ lib/ytl/importobj.rb
@@ -3,8 +3,15 @@ module YTL
   
   def self.import_ruby_object(context)
     context.import_object(Object, :Array, Array) 
+    context.import_object(Object, :Hash, Hash) 
     context.import_object(Object, :String, String)
+    context.import_object(Object, :Fixnum, Fixnum)
+    context.import_object(Object, :Float, Float)
+    context.import_object(Object, :Time, Time)
+    context.import_object(Object, :File, File)
+    context.import_object(Object, :IO, IO)
     context.import_object(Object, :Math, Math)
+    context.import_object(Object, :Exception, Exception)
   end
 end
   
